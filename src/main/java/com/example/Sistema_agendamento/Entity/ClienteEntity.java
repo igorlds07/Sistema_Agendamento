f43d0b4fda2a2b1c9c4c8d209de31f0d4339e9ce
Igor Lopes <igorrrlopesss8@gmail.com>
2025-09-13 01:03:24 -0300

    EnderecoEntity
    Implementei anotações e validações no EnderecoEntity.

--- src/main/java/com/example/Sistema_agendamento/Entity/ClienteEntity.java
+++ src/main/java/com/example/Sistema_agendamento/Entity/ClienteEntity.java
@@ -1,13 +1,19 @@
 package com.example.Sistema_agendamento.Entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity
 public class ClienteEntity extends PessoaEntity{
 
     @Email
+    @Column(name = "email", nullable = false, unique = true,  length = 100)
     private String email;
+
+    @NotBlank
+    @Column(name = "telefone")
     private String telefone;
 
 
