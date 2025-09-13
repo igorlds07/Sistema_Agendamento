7522bfa7c3582d789377a2c63132661b1de99a41
Igor Lopes <igorrrlopesss8@gmail.com>
2025-09-13 01:01:58 -0300

    AgendamentoEntity
    Implementei as anotações e validações no AgendamentoEntity

--- src/main/java/com/example/Sistema_agendamento/Entity/AgendamentoEntity.java
+++ src/main/java/com/example/Sistema_agendamento/Entity/AgendamentoEntity.java
@@ -1,12 +1,17 @@
 package com.example.Sistema_agendamento.Entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Past;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDate;
 import java.util.UUID;
 
 @Entity
+@Getter
+@Setter
 @Table(name = "Agendamentos")
 public class AgendamentoEntity {
 
@@ -14,26 +19,37 @@ public class AgendamentoEntity {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID idAgendamento;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "cliente_id", nullable = false)
     private ClienteEntity idCliente;
 
-    @OneToMany
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "tecnico_id", nullable = false)
     private TecnicoEntity idTecnico;
 
-    @Past
+    @NotNull
+    // @FutureOrPresent
+    @Column(name = "data_inicio", nullable = false)
     private LocalDate dataInicio;
 
-    @Past
+    @NotNull
+    // @FutureOrPresent
+    @Column(name = "data_fim", nullable = false)
     private LocalDate dataFim;
 
     @Enumerated(EnumType.STRING)
-    private enum status{
+    private Status status = Status.PENDENTE;
+
+    @OneToOne(mappedBy = "agendamento", fetch = FetchType.LAZY)
+    private OrdemServicoEntity ordemServico;
+
+    public enum Status{
         PENDENTE,
         CONFIRMADO,
         CANCELADO,
         CONCLUIDO
     }
-
 
 
 }
