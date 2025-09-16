b53644339b083eb0378ff6955100cb95495c3132
Igor Lopes <igorrrlopesss8@gmail.com>
2025-09-16 00:04:49 -0300

    Atualizações no AnexoEntity
    Implementei aas anotações do spring boot e validações para todos os campos da entity.

--- src/main/java/com/example/Sistema_agendamento/Entity/AnexoEntity.java
+++ src/main/java/com/example/Sistema_agendamento/Entity/AnexoEntity.java
@@ -1,18 +1,56 @@
 package com.example.Sistema_agendamento.Entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jdk.jfr.ContentType;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
+@Getter
+@Setter
 public class AnexoEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "ordem_servico_id", nullable = false)
     private OrdemServicoEntity ordemServico;
+
+    @NotBlank
+    @Column(name = "nome_arquivo", nullable = false, length = 150)
     private String nomeArquivo;
-    private ContentType contentType;
-    private Integer tamanho;
-    private LocalDate criado;
+
+    @NotBlank
+    @Column(name = "content_type", nullable = false, length = 100)
+    private String contentType;
+
+    @NotNull
+    @Column(name = "tamanho_bytes", nullable = false)
+    private long tamanhoBytes;
+
+    @DateTimeFormat
+    private LocalDateTime criado;
+
+    @NotBlank
+    @Column(name = "path", nullable = false, length = 400)
+    private String path;
+
+    @NotNull
+    @Column(name = "criado_em", nullable = false)
+    private LocalDateTime criadoEm;
+
+    @PrePersist
+    private void prePersist() {
+        if (criadoEm == null) criadoEm = LocalDateTime.now();
+    }
 
 }
